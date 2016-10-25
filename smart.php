415a8b2e4e977577f50972660247d93bfa3bbe5d
Chameera Lakshitha <chameera.lakshitha212@gmail.com>
2016-10-25 18:58:45 +0530

    Initialized

---
+++ smart.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Smart
+Plugin URI: #
+Description: Smart plugin is a wordpress plugin
+Version: 1.0
+Author: Lakshitha212
+Author URI: https://github.com/lakshitha212
+*/
+define('SMART_DIR', dirname(__FILE__));
+define('SMART_THEMES_DIR', SMART_DIR . "/themes");
+define('SMART_URL', WP_PLUGIN_URL . "/" . basename(SMART_DIR));
+define('SMART_VERSION', '1.0');
